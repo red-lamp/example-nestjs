e447f9406ff65975625dcb69e4a8bc28d7a94f19
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-11-25 18:30:43 +0700

    add dto and enum

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ReadAppDto } from './dto/read-app.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): ReadAppDto {
+    return this.appService.getApp();
   }
 }
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ReadAppDto } from './dto/read-app.dto';
+import { EnumApp } from './dto/enum.app';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  /**
+   * Assemble a dto read app
+   */
+  private assembleReadAppDto(): ReadAppDto {
+    const radto = new ReadAppDto();
+    radto.name = 'Online store';
+    radto.type = EnumApp.OnlineStore;
+    radto.description = 'P2 Online store';
+
+    return radto;
+  }
+
+  /**
+   * return a ReadAppDto object
+   */
+  getApp(): ReadAppDto {
+    return this.assembleReadAppDto();
   }
 }
