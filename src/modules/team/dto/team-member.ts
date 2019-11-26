60b2a432ccb3fb7e30aebff61c1b7e5a1c99bbb5
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-11-26 16:46:08 +0700

    add shared dto object between module

---
+++ src/modules/team/dto/team-member.ts
@@ -0,0 +1,13 @@
+/**
+ * @author Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
+ */
+
+ /**
+  * Team member object
+  */
+export class TeamMember {
+    name: string;
+    position: string;
+    age: number;
+    gender: string;
+  }
