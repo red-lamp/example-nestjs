a11988dd15eab66b28c4bf8192cc0957b14155f8
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-11-27 09:31:50 +0700

    add sample of http method (GET, POST, PUT, DELETE)

---
+++ src/modules/app/dto/create-app.dto.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
+ */
+
+ /**
+  * App data transfer object
+  */
+export class CreateAppDTO {
+    readonly name: string;
+    readonly type: number;
+    readonly description: string;
+  }
